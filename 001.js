906915139dfcce3174cf77228288aa53aad449dc
robleslf <142787149+robleslf@users.noreply.github.com>
2024-04-09 13:15:55 +0200

    Add files via upload

---
+++ 001.js
@@ -0,0 +1,12 @@
+// Dependiendo de la edad, te va a decir si puedes sacar o no el carné de conducir
+
+let edad= 10;
+
+if (edad<18) {
+    console.log("No puedes sacar el carné");
+}
+
+else {
+    console.log("Puedes sacar el carnet");
+}
+

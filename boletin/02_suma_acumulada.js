179a2ca487f0d5c28674c07125681b5d151b555f
robleslf <142787149+robleslf@users.noreply.github.com>
2024-04-25 11:52:42 +0200

    Add files via upload

---
+++ boletin/02_suma_acumulada.js
@@ -0,0 +1,6 @@
+let acumulador = 0
+
+for(let i = 1; i<101; i++){
+    acumulador = acumulador + i;
+    console.log("Atención vamos por el número "+i+" y llevamos acumulada la friolera de "+ acumulador);
+}
---
+++ boletin/04_clasificacion_de_temperaturas.js
@@ -0,0 +1,14 @@
+let temperatura = "6";
+
+if (temperatura < 10) {
+    console.log(temperatura+"ºC se considera una temperatura baja.")
+}
+
+else if (temperatura > 25) {
+    console.log(temperatura+"ºC se considera una temperatura alta.")
+}
+
+else {
+    console.log(temperatura+"ºC es una tempteratura media.")
+}
+

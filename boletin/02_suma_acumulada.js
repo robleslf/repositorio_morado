087b97c151930463e41a1dc5e2c75ecf5d5fd63b
“robleslf” <robleslf93@gmail.com>
2024-05-01 12:47:08 +0200

    confirmados

--- boletin/02_suma_acumulada.js
+++
@@ -1,9 +0,0 @@
-let acumulador = 0;
-let acumuladorMenosUno = 0;
-
-for(let i = 1; i<101; i++){
-    acumuladorMenosUno = acumulador;
-    acumulador = acumulador + i;
-    
-    console.log(i+"+"+(acumuladorMenosUno)+"="+acumulador);
-}
